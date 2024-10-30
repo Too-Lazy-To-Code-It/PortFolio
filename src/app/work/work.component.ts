9efd97a7b1a0e24a3570f724c229fcb3f15928a5
Too-Lazy-To-Code-It <2lazy2nameit@gmail.com>
2024-10-30 18:52:51 +0100

    work  component

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
 
+interface Project {
+  title: string;
+  description: string;
+  tech: string[];
+  links: { github?: string; external?: string };
+  image: string;
+}
+
 @Component({
   selector: 'app-work',
   standalone: true,
-  imports: [],
   templateUrl: './work.component.html',
-  styleUrl: './work.component.scss'
+  styleUrls: ['./work.component.scss']
 })
 export class WorkComponent {
-
+  featuredProjects:  Project[] = [
+    {
+      title: 'Halcyon Theme',
+      description: 'A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, Atom Package Manager, and npm.',
+      tech: ['VS Code', 'Sublime Text', 'Atom', 'iTerm2', 'Hyper'],
+      links: {
+        github: 'https://github.com/Too-Lazy-To-Code-It/PortFolio',
+        external: 'none'
+      },
+      image: 'https://placehold.in/200.avif'
+    },
+    {
+      title: 'Spotify Profile',
+      description: 'A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.',
+      tech: ['React', 'Styled Components', 'Express', 'Spotify API', 'Heroku'],
+      links: {
+        github: 'https://github.com/Too-Lazy-To-Code-It/PortFolio',
+        external: ''
+      },
+      image: 'https://placehold.in/200.avif'
+    }
+  ];
 }
